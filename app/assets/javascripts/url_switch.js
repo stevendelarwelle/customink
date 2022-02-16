4f2b1809bbb6abcde1a3405b1d772a48db9baba2
Steve <sdelarwelle@wspackaging.com>
2022-02-16 08:40:39 -0600

    remove alert for testing

--- app/assets/javascripts/url_switch.js
+++ app/assets/javascripts/url_switch.js
@@ -1,6 +1,5 @@
 $(document).on('turbolinks:load', function(){
     $('.oldlink').click(function(){
-        alert('Does this work?');
         $('.oldlink').hide();
         $('.newlink').show();
     });
